5b1cc417f3f51561712b1d9bc0647b2fa2b712b8
JSantos <jsantos@provertec.com.br>
2026-03-03 02:51:56 -0300

    fix: name Home

--- modules/Classes/service/classes-service.ts
+++ modules/Classes/service/classes-service.ts
@@ -1,41 +1,35 @@
-import { ClassesModel } from "@/models/classes";
-import { HttpServiceInterface } from "@/service/http-service";
+import { ClassesModel } from '@/models/classes'
+import { HttpServiceInterface } from '@/service/http-service'
 
 export class ClassesService {
-   constructor(
-      private httpService:HttpServiceInterface
-   ){}
+  constructor(private httpService: HttpServiceInterface) {}
 
-   public async listAllClasses(filters?:{schoolId?:string}):Promise<ClassesModel[]>{
-      let filtersString = ``
+  public async listAllClasses(filters?: { schoolId?: string }): Promise<ClassesModel[]> {
+    let filtersString = ``
 
-      if(filters && filters.schoolId){
-         filtersString = `?schoolId=${filters.schoolId}`
-      }
+    if (filters && filters.schoolId) {
+      filtersString = `?schoolId=${filters.schoolId}`
+    }
 
-      const response = await this.httpService.get<{classes:ClassesModel[]}>(`/classes${filtersString}`)
-      return response.classes
-   }
+    const response = await this.httpService.get<{ classes: ClassesModel[] }>(`/classes${filtersString}`)
+    return response.classes
+  }
 
-   public async deleteClassesById(id:string):Promise<void>{
-      await this.httpService.delete(`/classes/${id}`)
-   }
+  public async deleteClassesById(id: string): Promise<void> {
+    await this.httpService.delete(`/classes/${id}`)
+  }
 
-   public async createClass(payload: Omit<ClassesModel, 'id'>): Promise<ClassesModel> {
-      const response = await this.httpService.post<{ class: ClassesModel }>(
-         `/classes`,
-         { class: { ...payload, schoolId: payload.school } }
-      )
-      return response.class
-   }
+  public async createClass(payload: Omit<ClassesModel, 'id'>): Promise<ClassesModel> {
+    const response = await this.httpService.post<{ class: ClassesModel }>(`/classes`, {
+      class: { ...payload, schoolId: payload.school },
+    })
+    return response.class
+  }
 
-   public async editClass({id,academicYear,name,shift,school}: ClassesModel): Promise<ClassesModel> {
-      const payloadBody = {academicYear,name,shift,schoolId:school}
+  public async editClass({ id, academicYear, name, shift, school }: ClassesModel): Promise<ClassesModel> {
+    const payloadBody = { academicYear, name, shift, schoolId: school }
 
-      const response = await this.httpService.put<{ class: ClassesModel }>(
-         `/classes/${id}`,
-         { class: payloadBody }
-      )
-      return response.class
-   }
+    const response = await this.httpService.put<{ class: ClassesModel }>(`/classes/${id}`, { class: payloadBody })
+    return response.class
+  }
 }
